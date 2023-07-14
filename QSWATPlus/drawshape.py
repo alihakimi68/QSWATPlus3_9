01d372d7252eec7b0431173d3d5e3ec6d4125173
Optimus <hakimi.ali68@gmail.com>
2023-07-14 18:30:15 +0200

    2023/7/14 - handle_refresh_click modified completely, it will refresh the layer list and show the layers that are in Drawing group layers. It also updates the area and centroids.
    - get_selected_row is for the table view, if user clicks on a column entire row will be selected
    
    - User can delete the entire row in the table view.

--- QSWATPlus/drawshape.py
+++ QSWATPlus/drawshape.py
@@ -26,14 +26,26 @@
  ***************************************************************************
 """
 
-from qgis.PyQt.QtCore import QObject
+from qgis.PyQt.QtCore import QObject, Qt
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction, QMessageBox
+from qgis.PyQt.QtWidgets import QAction, QMessageBox, QToolBar, QVBoxLayout, QTableWidgetItem, \
+                                QAbstractItemView, QAbstractButton, QTableWidget, QStyledItemDelegate,\
+                                QPushButton, QMessageBox, QTableView
+
+from qgis.PyQt.QtWidgets import QApplication, QStyleOptionViewItem, QStyledItemDelegate
+
+from qgis.core import QgsProject, QgsVectorLayer, QgsGeometry
+
+from functools import partial
 
 from .drawshapedialog import drawshapedialog
 from .qdraw import Qdraw
 import os.path
+
+
+
+
 
 class drawshape(QObject):
 
@@ -71,6 +83,12 @@ class drawshape(QObject):
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
         self.first_start = None
+
+        self.digitizing_toolbar = None
+
+        self.project = QgsProject.instance()
+        self.output_directory = self.project.homePath() + '/drshapes/'
+
 
 
     # noinspection PyMethodMayBeStatic
@@ -170,7 +188,8 @@ class drawshape(QObject):
             icon_path,
             text=self.tr(u'Draw shape'),
             callback=self.run,
-            parent=self.iface.mainWindow())
+            parent=None)
+            # parent = self.iface.mainWindow())
 
         # will be set False in run()
         self.first_start = True
@@ -197,13 +216,12 @@ class drawshape(QObject):
 
         self.dlg.cShapeButton.clicked.connect(self.handle_button_click)
 
-        # Run the dialog event loop
-        # result = self.dlg.exec_()
-        # # See if OK was pressed
-        # if result:
-        #     # Do something useful here - delete the line containing pass and
-        #     # substitute with your code.
-        #     pass
+        self.dlg.toolButton_Refresh.clicked.connect(self.handle_refresh_click)
+
+        # Set the selection behavior to select entire rows
+        self.dlg.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        # self.disable_table_editing()
+
 
 
     def handle_button_click(self):
@@ -223,20 +241,24 @@ class drawshape(QObject):
             resNumber = 4
 
         if self.dlg.cShapeButton.isChecked():
+
             self.dlg.groupBox_selectcategory.setEnabled(False)
             self.dlg.groupBox_drawpolygon.setEnabled(True)
             self.dlg.cShapeButton.setText('Select Category / Draw : Draw')
 
             # self.iface.messageBar().pushMessage("Error", str(resNumber), level=2, duration=5)
-            qdraw_instance = Qdraw(self.iface, resNumber)
+            qdraw_instance = Qdraw(self.iface, resNumber,self.output_directory,self.project)
             self.dlg.dRectangleButton.clicked.connect(qdraw_instance.drawRect)
             self.dlg.dCircleButton.clicked.connect(qdraw_instance.drawCircle)
             self.dlg.dPolygonButton.clicked.connect(qdraw_instance.drawPolygon)
+
             result = self.dlg.exec_()
             if result:
                 # Do something useful here - delete the line containing pass and
                 # substitute with your code.
                 pass
+
+
         elif not self.dlg.cShapeButton.isChecked():
             self.dlg.groupBox_selectcategory.setEnabled(True)
             self.dlg.groupBox_drawpolygon.setEnabled(False)
@@ -245,4 +267,138 @@ class drawshape(QObject):
         else:
             self.iface.messageBar().pushMessage("Error", 'This is not right', level=2, duration=5)
 
+    def handle_refresh_click(self):
+        # Assuming you have a reference to the layer group
+        group_name = "Drawings"
+
+        root = QgsProject.instance().layerTreeRoot()
+        group = root.findGroup(group_name)
+        if group and group.nodeType() == 0:
+            group = QgsProject.instance().layerTreeRoot().findGroup(group_name)
+
+            # Clear existing data in the table widget
+            self.dlg.tableWidget.setRowCount(0)
+
+            # Get the list of layers within the layer group
+            layers = [layer.layer() for layer in group.findLayers()]
+
+            if layers:
+
+                # Iterate over each layer and extract desired columns
+                for layer in layers:
+                    # Get the attribute table
+                    fields = layer.fields()
+
+                    # Filter desired columns based on their indices
+                    desired_columns = [fields[i] for i in [0, 2, 3]]
+
+                    # Start editing the layer
+                    layer.startEditing()
+
+                    # Iterate over features and populate the table widget
+                    for feature in layer.getFeatures():
+                        geometry = feature.geometry()
+                        area = QgsGeometry.area(geometry)
+                        centroid = feature.geometry().centroid().asPoint()
+
+                        field_index = fields.indexFromName('Area')  # Index of the 'Area' field
+                        attributes = feature.attributes()  # Get the existing attribute values
+                        attributes[field_index] = area  # Update the attribute value
+
+                        field_index = layer.fields().indexFromName('CentroidX')  # Index of the 'Area' field
+                        attributes[field_index] = centroid.x()  # Update the attribute value
+
+                        field_index = layer.fields().indexFromName('CentroidY')  # Index of the 'Area' field
+                        attributes[field_index] = centroid.y()  # Update the attribute value
+
+                        feature.setAttributes(attributes)  # Set the updated attributes to the feature
+                        layer.updateFeature(feature)  # Save the changes to the shapefile
+
+                        row = []
+                        for column in desired_columns:
+                            # Get the attribute value for each desired column
+                            attr_value = feature.attribute(column.name())
+                            row.append(str(attr_value))
+                        # Add the row to the table widget
+                        self.dlg.tableWidget.insertRow(self.dlg.tableWidget.rowCount())
+                        for i, value in enumerate(row):
+                            item = QTableWidgetItem(value)
+                            self.dlg.tableWidget.setItem(self.dlg.tableWidget.rowCount()-1, i, item)
+                            # Create the delete link for the fourth column
+                            delete_link = QPushButton("Delete")
+                            delete_link.setProperty("row", self.dlg.tableWidget.rowCount() - 1)
+                            delete_link.clicked.connect(partial(MyTableWidget.delete_row_confirmation, delete_link))
+                            delete_link.setStyleSheet("QPushButton { color: blue; text-decoration: underline; }")
+                            self.dlg.tableWidget.setCellWidget(self.dlg.tableWidget.rowCount() - 1, 3, delete_link)
+
+                    # Commit the changes to the layer's attribute table
+                    layer.commitChanges(stopEditing=True)
+            else:
+                self.iface.messageBar().pushMessage("Error", 'There are no layers in Drawing group', level=2,
+                                                    duration=5)
+
+        else:
+            self.iface.messageBar().pushMessage("Error", 'There is no Drawing group', level=2,
+                                                duration=5)
+
+    def get_selected_row(self):
+        selected_items = self.dlg.tableWidget.selectedItems()
+
+        if selected_items:
+            # Assuming that all selected items belong to the same row
+            first_selected_item = selected_items[0]
+            selected_row = first_selected_item.row()
+            return selected_row
+        else:
+            return -1  # No row selected
+
+    # Disable editing for all cells in a QTableWidget
+    # def disable_table_editing(self):
+    #     rows = self.dlg.tableWidget.rowCount()
+    #     cols = self.dlg.tableWidget.columnCount()
+    #
+    #     for row in range(rows):
+    #         for col in range(cols):
+    #             item = self.dlg.tableWidget.item(row, col)
+    #             if item is not None:
+    #                 item.setFlags(item.flags() & ~Qt.ItemIsEditable)
+    #             else:
+    #                 self.dlg.tableWidget.setItem(row, col, QTableWidgetItem())
+    #                 self.dlg.tableWidget.item(row, col).setFlags(
+    #                     self.dlg.tableWidget.item(row, col).flags() & ~Qt.ItemIsEditable
+    #                 )
+
+
+class DeleteLinkDelegate(QStyledItemDelegate):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def paint(self, painter, option, index):
+        if index.column() == 3:
+            button = QAbstractButton(index.data(), self.parent())
+            button.setGeometry(option.rect)
+            button.clicked.connect(self.parent().delete_row_confirmation)
+            button.setAutoFillBackground(True)
+            button.setStyleSheet("QPushButton { color: blue; text-decoration: underline; }")
+
+        super().paint(painter, option, index)
+
+
+class MyTableWidget(QTableWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setItemDelegate(DeleteLinkDelegate(self))
+
+    def delete_row_confirmation(self):
+        button = self.sender()
+        table_widget = button.parent()
+        while not isinstance(table_widget, QTableWidget):
+            table_widget = table_widget.parent()
+        index = table_widget.currentIndex()
+        if index.isValid():
+            row = index.row()
+            reply = QMessageBox.question(table_widget, "Delete Row", "Are you sure you want to delete this row?",
+                                         QMessageBox.Yes | QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                table_widget.removeRow(row)
 
